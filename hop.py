6dda29e9ec67f7592c8756e545575128fcc54e6c
Bryan Marty <bxm156@case.edu>
2012-12-02 13:27:49 -0500

    Working on switching over to UDP, but still not getting a ICMP response

--- hop.py
+++ hop.py
@@ -25,7 +25,7 @@ def build_ip_header(s,num,ttl,host):
     ip_identification = num
     ip_fragment_offset = 0 
     ip_ttl = ttl
-    ip_protocol = 1 # 1 = ICMP
+    ip_protocol = 17 # 17 = UDP
     ip_checksum = 0 # Depending on implementation, the kernel or the hardware will calculate this for us :)
     ip_source = socket.inet_aton(source_ip)
     ip_destination = socket.inet_aton(host)
@@ -69,6 +69,23 @@ def calc_icmp_checksum(data):
     s = s + (s >> 16)
     return ~s & 0xffff
 
+def build_udp(sport,dport,data):
+    checksum = 0
+    length = 8 + len(data)
+    udp_header = struct.pack("!HHHH",sport,dport,length,checksum)
+    udp_packet = udp_header + data
+    #checksum = calc_udp_checksum(udp_packet)
+    #udp_header = struct.pack("!HHHH",sport,dport,length,checksum)
+    return udp_header + data
+
+def calc_udp_checksum(data):
+    s = 0
+    for i in range(0, len(data), 2):
+        w = (ord(data[i]) << 8) + ord(data[i+1])
+        s = s + w
+    s = (s  & 0xffff) + ( s >> 16)
+    s = s + (s >> 16)
+    return ~s & 0xffff
 ###########################
 ### Response Validation ###
 ###########################
@@ -94,7 +111,7 @@ def receive_ping(my_socket, packet_id, icmp_id, time_sent, timeout):
 def validate_icmp_response(response,sent_ip_id,sent_icmp_id):
     icmp_header = response[20:28] #Extract the ICMP Response
     icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_sequence = struct.unpack('!bbHHh', icmp_header)
-    if icmp_type == 0 and icmp_code == 0 and icmp_id == sent_icmp_id:
+    if icmp_type == 3 and icmp_code == 3:
         #We have recieved a valid ICMP response!
         return (True,True)
 
@@ -106,10 +123,10 @@ def validate_icmp_response(response,sent_ip_id,sent_icmp_id):
     original_icmp_header = response[48:56]
     (original_icmp_type, original_icmp_code, original_icmp_checksum, original_icmp_id,
         original_icmp_seq) = struct.unpack('!bbHHh',original_icmp_header)
-    if icmp_type == 11 and icmp_code == 0 and original_icmp_id == sent_icmp_id and ip_ident == sent_ip_id:
+    if icmp_type == 11 and icmp_code == 0 and ip_ident == sent_ip_id:
         #The TTL Expired!
         return (True,False)
-
+    print icmp_type, icmp_code
     #Throw away
     return (False, False)
 
@@ -153,7 +170,7 @@ def run_search(s,host):
 
     while True:
         # Build Packet
-        packet = build_ip_header(s,starting_packet_id,ttl,host) + build_icmp(starting_icmp_id)
+        packet = build_ip_header(s,starting_packet_id,ttl,host) + build_udp(5337,65535,192*"Q")
         s.sendto(packet,("1.3.3.7",0)) #destination host doesn't matter, we make our own ip header
         (success,rtt) = receive_ping(s,starting_packet_id,starting_icmp_id, time.time(), TIMEOUT_PER_PING)
         if rtt is not False:
@@ -181,7 +198,7 @@ def run_search(s,host):
 def run(host):
     local_ip = socket.gethostbyname(socket.gethostname())
     
-    s = socket.socket(socket.AF_INET,socket.SOCK_RAW, ICMP_PROTO)
+    s = socket.socket(socket.AF_INET,socket.SOCK_RAW, socket.IPPROTO_UDP)
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     print run_search(s,host)
     lat1,long1 = get_coordinates(local_ip)
@@ -200,5 +217,5 @@ def run(host):
     s.close()
 
 if __name__ == "__main__":
-    run("199.255.189.60")
+    run("74.125.134.101")
 
